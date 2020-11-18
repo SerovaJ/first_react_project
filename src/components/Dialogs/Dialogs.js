58d686aca3ad0fba6680c8accf8dec96af35a15f
yuserova <yuserova@samsonpost.ru>
2020-11-18 14:43:35 +0300

    Отделение данных от UI, преобразование массива данных в массив компонентов

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,32 +3,44 @@ import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const DialogItem = (props) => {
-	let path = "/dialogs" + props.id;
+	let path = "/dialogs/" + props.id;
 
-	return <div className={s.dialog}>
-		<NavLink to={path}>{props.name}</NavLink>
+	return <div className = {s.dialog}>
+		<NavLink to = {path}>{props.name}</NavLink>
 	</div>
 }
 
 const Message = (props) => {
-	return <div className={s.message}>{props.message}</div>
+	return <div className = {s.message}>{props.message}</div>
 }
 
 const Dialogs = () => {
+	let dialogs = [
+		{id: 1, name: 'Juliya'},
+		{id: 2, name: 'Anton'},
+		{id: 3, name: 'Sofiya'},
+		{id: 4, name: 'Valeriya'},
+	]
+
+	let messages = [
+		{id: 1, message: 'Hi'},
+		{id: 2, message: 'Hello'},
+		{id: 3, message: 'Aloha'},
+		{id: 4, message: 'How are you?'},
+	]
+
+	let dialogsElements = dialogs.map (d => <DialogItem name = {d.name} id = {d.id} />);
+
+	let messagesElements = messages.map (m => <Message message = {m.message} />);
+
 	return (
-		<div className={s.dialogs}>
-			<div className={s.items}>
-				<DialogItem name="Juliya" id="1" />
-				<DialogItem name="Anton" id="2" />
-				<DialogItem name="Sofiya" id="3" />
-				<DialogItem name="Valeriya" id="4" />
+		<div className = {s.dialogs}>
+			<div className = {s.items}>
+				{dialogsElements}
 			</div>
 
-			<div className={s.messages}>
-				<Message message="Hi" />
-				<Message message="Hello" />
-				<Message message="Aloha" />
-				<Message message="How are you?" />
+			<div className = {s.messages}>
+				{messagesElements}
 			</div>
 		</div>
 	);
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,33 +3,34 @@ import s from "./MyPosts.module.css";
 import Post from './Post/Post';
 
 const MyPosts = () => {
-  return(
-    
-        <div className={s.posts}>
-          <h2>My posts</h2>
-          <form className={s.form}>
-            <p>
-              <textarea
-                type="text"
-                name="my_news"
-                rows="5"
-                placeholder="Your news..."
-              ></textarea>
-            </p>
-            <div className={s.wrapper_button}>
-              <button className="Button" type="submit">
-                Send
-              </button>
-            </div>
-          </form>
-          
-            <Post message="It's your new program!" like="7"/>
-            <Post message="Hello!" like="5"/>
-            
-         
-        </div>
-     
-   
-  );
+	let posts = [
+		{id: 1, message: 'It\'s your new program!', likesCount: 12},
+		{id: 2, message: 'Hello!', likesCount: 11},
+	]
+
+	let postsElements = posts.map (p => <Post message = {p.message} like = {p.likesCount} />)
+
+	return (
+		<div className = {s.posts}>
+			<h2>My posts</h2>
+			<form className = {s.form}>
+			<p>
+				<textarea
+					type="text"
+					name="my_news"
+					rows="5"
+					placeholder="Your news..."
+				></textarea>
+			</p>
+			<div className = {s.wrapper_button}>
+				<button className="Button" type="submit">
+					Send
+				</button>
+			</div>
+			</form>
+			{postsElements}
+		</div>
+	);
 }
+
 export default MyPosts;
